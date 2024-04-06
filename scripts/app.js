8845ddc62286be06f8a1a5e091aa030e17c81526
rasool1992 <somanxx@yahoo.com>
2024-04-07 01:13:03 +0300

    update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,30 @@
 const cityForm = document.querySelector(".location");
 const card = document.querySelector(".card");
+
+const API_KEY = "953677dccfcd4b14b3a185020240604";
+
+async function getWeather(id) {
+  const BASE_URL = `http://api.weatherapi.com/v1/current.json`;
+  let query = `?key=${API_KEY}&q=id:${id}`;
+  let response = await fetch(`${BASE_URL}${query}`);
+  let data = await response.json();
+  return data.current;
+}
+
+async function getCity(city) {
+  const BASE_URL = `http://api.weatherapi.com/v1/search.json`;
+  let query = `?key=${API_KEY}&q=${city}`;
+
+  let response = await fetch(`${BASE_URL}${query}`);
+  let data = await response.json();
+  return data[0];
+}
+getCity("baghdad")
+  .then(function (data) {
+    return getWeather(data.id);
+  })
+  .catch(function (err) {});
+
 async function updateUI(data) {
   let { cityDetails, cityWeather } = data;
   let htmlContent = `
